aaa4cbdc2fbb51dac343fa053f586c55bc3903c5
barpacha <bar2012p@mail.ru>
2020-05-01 22:29:38 +0700

    0.1

---
+++ lab3/lab3/Source.cpp
@@ -0,0 +1,29 @@
+
+template <typename T, typename key = int> class BST
+{
+	struct node
+	{
+		T data;
+		key key;
+		node *left;
+		node *right;
+	};
+	
+public:
+	BST();
+	~BST();
+};
+
+template<typename T, typename key>
+BST<T, key>::BST()
+{
+}
+
+template<typename T, typename key>
+BST<T, key>::~BST()
+{
+}
+
+void main()
+{
+}
